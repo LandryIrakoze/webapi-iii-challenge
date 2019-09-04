f8e13d8a4e212bba4f2afb12d35fbd062a05c86b
Landry Irakoze <irakozel@gmail.com>
2019-09-04 15:23:10 -0400

    built middleware

--- server.js
+++ server.js
@@ -9,7 +9,40 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log({
+    'req': req,
+    'res': res,
+  })
+  next();
 };
+
+function validateUserId(req, res, next) {
+  let { id } = req.params;
+  if (id) {
+    req.user = id;
+  } else {
+    res.status(400).json({ message: 'invalid user id' })
+  }
+  next();
+};
+
+function validateUser(req, res, next) {
+  let { body } = req.body;
+  if (!body) {
+    res.status(400).json({ message: 'missing user data' })
+  } else if (!body.name) {
+    res.status(400).json({ message: 'missing required name field' })
+  }
+  next();
+}
+
+function validatePost(req, res, next) {
+  let { body } = req.body;
+  if (!body) {
+    res.status(400).json({ message: 'missing post data '})
+  } else if (!body.text) {
+    res.status(400).json({ message: 'missing required text field' })
+  }
+}
 
 module.exports = server;
