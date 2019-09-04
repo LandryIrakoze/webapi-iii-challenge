7a624ad7ef58d8643df50913f4c1a79bc20f70c0
Landry Irakoze <irakozel@gmail.com>
2019-09-04 15:40:06 -0400

    added routes

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+//global middleware
+server.use(express.json());
 
+server.use('/api/user', userRouter);
+server.use('/api/posts', postRouter);
+
+//custom middleware
 function logger(req, res, next) {
   console.log({
     'req': req,
